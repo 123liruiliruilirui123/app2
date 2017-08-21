3cf9af5f56916e66e56bd721986885616d6ea42e
lirui <1805622624@qq.com>
2017-08-21 10:29:46 +0800

    完成登录页布局

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
 $(function() {
-
+    //输入框的点击事件
+    $('.inneed').click(function() {
+        $('.inneed-hide').show();
+    })
+    $('.inneed').blur(function() {
+        $('.inneed-hide').hide();
+    })
+    //文字部分
+    $('.one icon-jia').click(function() {
+        $('.one dt').eq(1).show();
+        $(this).class.add('icon-iconjian');
+        $(this).class.remove('icon-jia');
+    })
 })
